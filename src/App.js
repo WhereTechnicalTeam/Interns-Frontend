18bc1264b5ae19a8a34237f6055fc72f7b29b663
faith <fmuchembi@gmail.com>
2021-10-30 12:23:39 +0300

    :tada: initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends React.Component{
+  render(){
+    return(
+      <div className="container">
+        App
+      </div>
+    );
+  }
+}
+export default App;
+
+
