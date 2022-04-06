8194d331a4692401aa93759e203c31050bfc63e0
faith <fmuchembi@gmail.com>
2022-04-06 21:50:31 +0300

    remove comments

--- src/components/Map.js
+++ src/components/Map.js
@@ -7,6 +7,8 @@ import marker from '../assets/images/star-solid.svg';
 
 
 
+
+
 class Map extends React.Component{
     render(){
         const style = {
@@ -20,20 +22,17 @@ class Map extends React.Component{
         iconSize:[12,12]
     });
       const styles= {
-        fillColor:'none',
+        fillColor:'lime',
         weight: 0.5,
         opacity: 1,
-        color:'yellow',
+        color:'lime',
         dashArray: 1,
         fillOpacity: 0.7
     }
     const img ={
-        height:'100px',
+        height:'100%',
         width:"100px",
         marginTop:"5%"
-    }
-    const viewImage = ()=>{
-        alert('hello')
     }
     const limeOptions = { color: 'lime', weight:2, }
     const position = [0,0]
@@ -44,24 +43,31 @@ class Map extends React.Component{
                         attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
                         url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
                     />
-                     {this.props.pointData.map( place => {
+                    {this.props.buttonId === 0 && <div className="container">
+                    {this.props.pointData.map( place => {
                         const point = [place.geometry.coordinates[1], place.geometry.coordinates[0]];
                         console.log(point)
                         return (
                             <Marker position={point} key={place.id} icon={myIcon} >
                                 <Popup>
                                     <div className="popupdiv">
-                                    {place.properties.name}
-                                    <img src="https://images.unsplash.com/photo-1648793633175-f3635585014b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" style={img} onClick={viewImage}/>
+                                    <div>{place.properties.name}</div>
+                                    <div>{place.properties.desc}</div>
+                                    <div><img src="https://th.bing.com/th/id/R.14cbd2a7d160c5ea940f9ddcf74b6572?rik=K674DAW42U%2f6hw&riu=http%3a%2f%2ficdn.images.touristlink.com%2frepository%2fMA%2fIN%2fEN%2fTR%2fAN%2fC%2funiversity-of-ghana-main-entrance-to-the-balme-lib.jpg&ehk=3racseq%2f57MCkxDHdrITHkL%2foATjve0qrd4hKLGECtY%3d&risl=&pid=ImgRaw&r=0" style={img} alt={place.properties.name}/></div>
+                                    <div><img src="https://th.bing.com/th/id/R.96e5d084b49ee0e3a475ad85d8faafd0?rik=Oj5sdTlIaGqs7Q&riu=http%3a%2f%2f4.bp.blogspot.com%2f-tj30pcxDs5c%2fUUczFYsWyPI%2fAAAAAAAAA-Q%2f4wTVkRJpD1w%2fs1600%2fUniversity%2bof%2bGhana%2b%2b2191.jpg&ehk=uYsdH%2f5%2fPjtPjnSH3ERS3ndlV%2fnP07Ftvrzlti8FnDE%3d&risl=&pid=ImgRaw&r=0" style={img} alt={place.properties.name}/></div>
                                     </div>
                                 </Popup>
                             </Marker>
                         );
-                    })}
+                    })} </div>}
+                    {this.props.buttonId === 1 && <div className="container">
                     {this.props.lineData.map((lines) => {
                      return <Polyline key={lines.id} positions={lines.geometry.coordinates} pathOptions={limeOptions} />
                         })}
-                    <GeoJSON data={this.props.polygonData} style={styles}/>
+                    </div>}
+                    {this.props.buttonId === 2 && <div className="container">
+                      <GeoJSON data={this.props.polygonData} style={styles}/>
+                    </div>}
                 </MapContainer>
             </div>
         )
--- src/components/TableFormData.js
+++ src/components/TableFormData.js
@@ -2,34 +2,25 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-const  TableFormData = ({pointData, lineData, polygonData})=>{
 
-      return (
-        <div className="container">
-            <table className="table table-bordered">
-                <thead>
-                <tr>
-                    <th scope="col">id</th>
-                    <th scope="col">Type</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Description</th>
-                    <th scope="col">coordinates</th>
-                </tr>
-                </thead>
-               <tbody>{
-                        pointData.map( item => (
-                            <tr key={item.id}>
-                                <td>{item.id}</td>
-                                <td>{item.geometry.type}</td>
-                                <td>{item.properties.name}</td>
-                                <td>{item.properties.desc}</td>
-                                <td>{item.geometry.coordinates}</td>
-                            </tr>
-                        ))
-                    }
-                </tbody>
+class TableFormData extends React.Component{
+   
+    render(){
+        return(
+            <div>
+                {this.props.buttonId === 0 && <div className="container">
+                <table className="table table-bordered">
+                    <thead>
+                    <tr>
+                        <th scope="col">id</th>
+                        <th scope="col">Type</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">coordinates</th>
+                    </tr>
+                    </thead>
                 <tbody>{
-                        lineData.map( item => (
+                        this.props.pointData.map( item => (
                             <tr key={item.id}>
                                 <td>{item.id}</td>
                                 <td>{item.geometry.type}</td>
@@ -38,10 +29,48 @@ const  TableFormData = ({pointData, lineData, polygonData})=>{
                                 <td>{item.geometry.coordinates}</td>
                             </tr>
                         ))
-                    }
-                </tbody>
+                        }
+                    </tbody>
+                </table>
+            </div>}
+                {this.props.buttonId === 1 && <div className="container">
+                <table className="table table-bordered">
+                    <thead>
+                    <tr>
+                        <th scope="col">id</th>
+                        <th scope="col">Type</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">coordinates</th>
+                    </tr>
+                    </thead>
                 <tbody>{
-                        polygonData.features.map( item => (
+                        this.props.lineData.map( item => (
+                            <tr key={item.id}>
+                                <td>{item.id}</td>
+                                <td>{item.geometry.type}</td>
+                                <td>{item.properties.name}</td>
+                                <td>{item.properties.desc}</td>
+                                <td>{item.geometry.coordinates}</td>
+                            </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div>}
+            {this.props.buttonId === 2 && <div className="container">
+                <table className="table table-bordered">
+                    <thead>
+                    <tr>
+                        <th scope="col">id</th>
+                        <th scope="col">Type</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">coordinates</th>
+                    </tr>
+                    </thead>
+                    <tbody>{
+                        this.props.polygonData.features.map( item => (
                             <tr key={item.id}>
                                 <td>{item.id}</td>
                                 <td>{item.geometry.type}</td>
@@ -52,8 +81,24 @@ const  TableFormData = ({pointData, lineData, polygonData})=>{
                         ))
                     }
                 </tbody>
-            </table>
-        </div>
-    );
+                </table>
+            </div>}
+            {this.props.buttonId !== 0 && this.props.buttonId !== 1 && this.props.buttonId !== 2 && <div className="container">
+                <table className="table table-bordered">
+                    <thead>
+                    <tr>
+                        <th scope="col">id</th>
+                        <th scope="col">Type</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">coordinates</th>
+                    </tr>
+                    </thead>
+                </table>
+            </div>}
+            </div>
+        );
+        
+    }
 }
 export default TableFormData;
